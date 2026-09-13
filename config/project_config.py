953fba0d087f3c075d999073bb95849c14aa71cb
Raushan225024 <raushankumar225024@gmail.com>
2026-09-13 17:26:45 +0530

    add project config

---
+++ config/project_config.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+
+
+load_dotenv()
+# base path
+BASE_DIR = Path(__file__).resolve().parent.parent
+print(f"BASE_DIR: {BASE_DIR}")
+# -------------------------
+# API Keys
+# -------------------------
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# -------------------------
+# LLM Configuration
+# -------------------------
+LLM_MODEL = "llama-3.3-70b-versatile"
+
+# -------------------------
+# Embedding Model
+# -------------------------
+EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
+# -------------------------
+# Document Configuration
+# -------------------------
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 200
+
+# -------------------------
+# Data Directory
+# -------------------------
+DATA_DIR = BASE_DIR / "data"
+
+COLLECTION_NAME = "documents"
+
+documents = None 
+chunks = None
+
+embedding_model = None
+vectors = None
