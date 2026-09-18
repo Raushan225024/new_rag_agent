1b048c8430345b23909c125106ad0a2a1edaf225
Raushan225024 <raushankumar225024@gmail.com>
2026-09-18 12:59:18 +0530

    comfiger llm call

---
+++ llmcall/llmcall.py
@@ -0,0 +1,50 @@
+import os
+
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+
+llm = ChatGroq(
+    model="openai/gpt-oss-20b",
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+prompt = ChatPromptTemplate.from_template(
+    """
+You are an AI assistant representing Raushan Kumar.
+
+The user is asking questions about Raushan Kumar.
+
+IMPORTANT:
+When the user says "you", "your", or "yourself",
+interpret it as referring to Raushan Kumar.
+
+Answer ONLY using the provided context.
+
+Context:
+{context}
+
+Question:
+{question}
+
+
+Answer:
+"""
+)
+
+
+def ask_llm(question, docs):
+
+    context = "\n\n".join(
+        doc["content"] for doc in docs
+    )
+
+    chain = prompt | llm
+
+    response = chain.invoke(
+        {
+            "context": context,
+            "question": question,
+        }
+    )
+
+    return response.content
