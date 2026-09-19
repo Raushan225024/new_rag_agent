a6d794b66db5dd7f55669164df00cbbbdce3aea8
Raushan225024 <raushankumar225024@gmail.com>
2026-09-19 20:51:36 +0530

    add vercel url

--- create_api.py
+++ create_api.py
@@ -48,7 +48,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://bookingappui-8fgf.vercel.app/"
+        "https://ai-mern-eight.vercel.app/"
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],
