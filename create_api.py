5bbc7f647fefdf18d7d2be13f4c05bc74da2db50
Raushan225024 <raushankumar225024@gmail.com>
2026-09-19 20:06:20 +0530

    add vercel ui url in the origin for cors middleware

--- create_api.py
+++ create_api.py
@@ -48,6 +48,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://bookingappui-8fgf.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
